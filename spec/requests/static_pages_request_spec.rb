d207eabde24a2a8cf82c567acdb711bffc442d0d
Daisuke Kojima <kumasanjp2019@gmail.com>
2020-06-24 16:22:19 +0900

    StaticPages titleの設定

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  let(:base_title) { "InstalikeApp" }
+  it "is get home" do
+    get root_url
+    expect(response).to have_http_status(200)
+    assert_select "title", "#{base_title}"
+  end
 
+  it "is get help" do
+    get help_url
+    expect(response).to have_http_status(200)
+    assert_select "title", "#{base_title}"
+  end
+
+  it "is get about" do
+    get about_url
+    expect(response).to have_http_status(200)
+    assert_select "title", "#{base_title}"
+  end
+
+  it "is get contact" do
+    get contact_url
+    expect(response).to have_http_status(200)
+    assert_select "title", "#{base_title}"
+  end
 end
