f521afbd7dee79234d4862bc02e75b2dd947516c
Daisuke Kojima <kumasanjp2019@gmail.com>
2020-06-25 00:39:13 +0900

    Userモデルのバリデーション設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validation" do
+    let(:user) { User.new(name: "Example User", email: "user@example.com",
+                          password: "hogehoge", password_confirmation: "hogehoge") }
+    
+    context "Valid Case" do
+      example "Standard data" do
+        expect(user).to be_valid
+      end
+    end
+
+    context "Invalid Case" do
+      example "Blank name" do
+        user.name = "  "
+        expect(user).not_to be_valid
+      end
+
+      example "Blank email" do
+        user.email = "  "
+        expect(user).not_to be_valid
+      end
+
+      example "Too long name" do
+        user.name = "a" * 51
+        expect(user).not_to be_valid
+      end
+
+      example "Too long email" do
+        user.email = "a" * 256
+        expect(user).not_to be_valid
+      end
+
+      example "Non-conformity email" do
+        invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
+        invalid_addresses.each do |invalid_address|
+          user.email = invalid_address
+          expect(user).not_to be_valid
+        end
+      end
+
+      example "Registered email" do
+        duplicate_user = user.dup
+        user.save
+        expect(duplicate_user).not_to be_valid
+      end
+
+      example "Blank password" do
+        user.password = user.password_confirmation = " " * 8
+        expect(user).not_to be_valid
+      end
+
+      example "Too short password" do
+        user.password = user.password_confirmation = "a" * 7
+        expect(user).not_to be_valid
+      end
+    end
+  end
 end
--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -5,25 +5,25 @@ RSpec.describe "StaticPages", type: :request do
 
   it "is get root" do
     get root_url
-    expect(response).to have_http_status(200)
+    expect(response).to have_http_status(:success)
     assert_select "title", "#{base_title}"
   end
 
   it "is get help" do
     get help_url
-    expect(response).to have_http_status(200)
+    expect(response).to have_http_status(:success)
     assert_select "title", "Help | #{base_title}"
   end
 
   it "is get about" do
     get about_url
-    expect(response).to have_http_status(200)
+    expect(response).to have_http_status(:success)
     assert_select "title", "About | #{base_title}"
   end
 
   it "is get contact" do
     get contact_url
-    expect(response).to have_http_status(200)
+    expect(response).to have_http_status(:success)
     assert_select "title", "Contact | #{base_title}"
   end
 end
