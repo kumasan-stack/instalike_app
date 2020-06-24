8ce5fa30eccd5127ae029dc797bd158b34172c2e
Daisuke Kojima <kumasanjp2019@gmail.com>
2020-06-24 16:31:33 +0900

    タイトルヘルパー実装

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   let(:base_title) { "InstalikeApp" }
-  it "is get home" do
+
+  it "is get root" do
     get root_url
     expect(response).to have_http_status(200)
     assert_select "title", "#{base_title}"
@@ -11,18 +12,18 @@ RSpec.describe "StaticPages", type: :request do
   it "is get help" do
     get help_url
     expect(response).to have_http_status(200)
-    assert_select "title", "#{base_title}"
+    assert_select "title", "Help | #{base_title}"
   end
 
   it "is get about" do
     get about_url
     expect(response).to have_http_status(200)
-    assert_select "title", "#{base_title}"
+    assert_select "title", "About | #{base_title}"
   end
 
   it "is get contact" do
     get contact_url
     expect(response).to have_http_status(200)
-    assert_select "title", "#{base_title}"
+    assert_select "title", "Contact | #{base_title}"
   end
 end
