a7e7020ed62ce057f96314e70fd54965566cca80
Daisuke Kojima <kumasanjp2019@gmail.com>
2020-07-01 10:13:06 +0900

    facebookログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,12 @@ class User < ApplicationRecord
     clean_up_passwords
     result
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, skip: :sessions,  path_names: { sign_up: 'signup' },
-              controllers: { registrations: 'users/registrations' }
+              controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     get    'login',         to: 'devise/sessions#new',       as: :new_user_session
     post   'login',         to: 'devise/sessions#create',    as: :user_session
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
     get    'edit/password', to: 'devise/registrations#edit', as: :edit_password_user_registration
   end
   root   "static_pages#home"
-  get    "/terms",   to: "static_pages#terms"
-  get    "/policy",  to: "static_pages#policy"
-  get    "/contact", to: "static_pages#contact"
+  get    "/terms",          to: "static_pages#terms"
+  get    "/policy",         to: "static_pages#policy"
+  get    "/contact",        to: "static_pages#contact"
   resources :users, only: [:index, :show]
 end
