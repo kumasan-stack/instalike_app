f72d01805e811a178f61fb26da22ac49f66f51d6
Daisuke Kojima <kumasanjp2019@gmail.com>
2020-07-05 09:12:38 +0900

    フォロー機能のAjax化

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module InstalikeApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # 認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
