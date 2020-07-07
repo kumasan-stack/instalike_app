6c200dc87895f3560afae14fe5f77222abcd5ebf
Daisuke Kojima <kumasanjp2019@gmail.com>
2020-07-07 15:22:33 +0900

    ログインページをrootに統合

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, skip: :sessions,  path_names: { sign_up: 'signup' },
               controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
-    get    'login',         to: 'devise/sessions#new',       as: :new_user_session
-    post   'login',         to: 'devise/sessions#create',    as: :user_session
+    post   '/',             to: 'devise/sessions#create',    as: :user_session
     delete 'logout',        to: 'devise/sessions#destroy',   as: :destroy_user_session
     get    'edit/password', to: 'devise/registrations#edit', as: :edit_password_user_registration
   end
