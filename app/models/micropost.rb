8747d1504ec01d59da1de8962701556ddc46457d
Daisuke Kojima <kumasanjp2019@gmail.com>
2020-07-08 10:21:08 +0900

    micropost仕様変更(image必須,content任意,image正方形表示)

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,14 +3,16 @@ class Micropost < ApplicationRecord
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
-  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+  validates :content, length: { maximum: 50 }
+  validates :image,   presence: true,
+                      content_type: { in: %w[image/jpeg image/png],
                                       message: "must be a valid image format" },
                       size:         { less_than: 5.megabytes,
                                       message: "should be less than 5MB" }
 
-  # 表示用のリサイズ済み画像を返す
-  def display_image
-    image.variant(resize_to_limit: [500, 500])
+  # 表示用の正方形リサイズ済み画像を返す
+  def display_square_image(side_length: 500)
+    size = "#{side_length}x#{side_length}"
+    image.variant(combine_options:{resize:"#{size}^",crop:"#{size}+0+0",gravity: :center})
   end
 end
