8283526b18bffb4cc633120f93917729ef6d786f
Daisuke Kojima <kumasanjp2019@gmail.com>
2020-07-10 22:27:32 +0900

    ログイン前にUser#showアクションを呼び出すとエラーが出るバグを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index, :following, :followers]
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
 end
