0c840e84ec8eb44b23dadf73af49ae75803cdbcf
Daisuke Kojima <kumasanjp2019@gmail.com>
2020-07-12 12:32:26 +0900

    rails g controller Favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
-  has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name: "Relationship",
-            foreign_key: "follower_id", dependent: :destroy
+  has_many :microposts,             dependent: :destroy
+  has_many :favorites,              dependent: :destroy
+  has_many :active_relationships,  class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                    dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship",
-            foreign_key: "followed_id", dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
-  has_many :favorites, dependent: :destroy
-  has_many :favorite_post, through: :favorites, source: :micropost
+                                  foreign_key: "followed_id",
+                                    dependent: :destroy
+  has_many :following,     through: :active_relationships,  source: :followed
+  has_many :followers,     through: :passive_relationships, source: :follower
+  has_many :favorite_post, through: :favorites,             source: :micropost
   devise :database_authenticatable, :registerable,
           :rememberable, :validatable,:omniauthable
   before_save { email.downcase! }
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   resources :users,         only: [:index,  :show]
   resources :microposts,    only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites,     only: [:create, :destroy]
 end
