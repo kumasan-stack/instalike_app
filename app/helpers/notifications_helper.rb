c5b3adc7a0b12b0f28311c05a6a3a87e582bf41a
Daisuke Kojima <kumasanjp2019@gmail.com>
2020-07-15 00:28:10 +0900

    コードの改行位置等を修正

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,12 @@
 module NotificationsHelper
+  def activity_message(activity)
+    case activity 
+    when "Favorite" then 
+      "をお気に入りに追加しました" 
+    when "Comment" then 
+      "にコメントしました" 
+    else
+      ""
+    end 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users, skip: :sessions,  path_names: { sign_up: 'signup' },
-              controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
+              controllers: { registrations: 'users/registrations',
+                        omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     post   '/',             to: 'users/sessions#create',     as: :user_session
     delete 'logout',        to: 'users/sessions#destroy',    as: :destroy_user_session
     get    'edit/password', to: 'users/registrations#edit',  as: :edit_password_user_registration
   end
+
   root   "static_pages#home"
   get    "terms",           to: "static_pages#terms"
   get    "policy",          to: "static_pages#policy"
+
   resources :users,         only: [:index,  :show] do
     member do
       get :following, :followers
     end
   end
+
   resources :microposts,    only: [:new, :show, :index, :create, :destroy]
   resources :relationships, only: [:create, :destroy]
   resources :favorites,     only: [:create, :destroy]
