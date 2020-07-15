e06f39b551f04536dec423412d56aa3b476d62be
Daisuke Kojima <kumasanjp2019@gmail.com>
2020-07-15 14:58:48 +0900

    seeds.rbの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create a main sample user.
+User.create!( name:                  "豊臣秀吉",
+              user_name:             "サルゲッチュ",
+              email:                 "example@railstutorial.org",
+              password:              "password",
+              password_confirmation: "password")
+
+# Generate a bunch of additional users.
+99.times do |n|
+  name  =     Faker::Name.name
+  user_name = Faker::Superhero.name
+  email =     "example-#{n+1}@railstutorial.org"
+  password =  "password"
+  profile =   Faker::ChuckNorris.fact
+  User.create!( name:                  name,
+                user_name:             user_name,
+                email:                 email,
+                password:              password,
+                password_confirmation: password,
+                profile:               profile)
+end
+
+# Create following relationships.
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
